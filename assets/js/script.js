ccd05bc4aa078367a39b8a277ac41ad9eb1d7c1c
Mary Young <itsmaryyyyy@gmail.com>
2024-05-30 20:35:09 +0000

    add code to calculate two random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function runGame() {
 
+    let num1 = Math.floor(math.random() * 25) + 1;
+    let num2 = Math.floor(math.random() * 25) + 1;
 }
 
 function checkAnswer() {
